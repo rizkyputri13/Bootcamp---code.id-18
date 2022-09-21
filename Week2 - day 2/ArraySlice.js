844601e73c090e9998ee056978c5e7394372d80b
rizkyputri13 <74160733+rizkyputri13@users.noreply.github.com>
2022-09-21 14:06:55 +0700

    Delete Week2 - day 2 directory

--- Week2 - day 2/ArraySlice.js
+++
@@ -1,5 +0,0 @@
-const programming = ['java','javascript','python','sql','oracle','postgress']
-
-const db = programming.slice(3,5)
-
-console.log(db);
--- Week2 - day 2/ArraySplice.js
+++
@@ -1,5 +0,0 @@
-const programming = ['java','javascript','python','sql','oracle','postgress']
-
-const db = programming.splice(3,1)
-
-console.log(programming);
